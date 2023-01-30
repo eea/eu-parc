07ccfdf9002891ec267831ab61e0bc1459b04edf
Max <max@selk.global>
2023-01-30 10:58:08 +0200

    news teaser same height

--- web/themes/custom/parc/js/script.js
+++ web/themes/custom/parc/js/script.js
@@ -57,7 +57,7 @@
       // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
       document.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
         var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
-        console.log(st);
+
         if (st > lastScrollTop && st > 120){
           $('#header').addClass('vh');
         } else {
