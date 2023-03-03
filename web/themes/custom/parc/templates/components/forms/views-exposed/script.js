5d83b232fc9d853bdef92c0d0839f4ad7426a416
Max <max@selk.global>
2023-03-03 10:56:16 +0200

    Event date sorting

--- web/themes/custom/parc/templates/components/forms/views-exposed/script.js
+++ web/themes/custom/parc/templates/components/forms/views-exposed/script.js
@@ -8,12 +8,13 @@
     attach: function (context, settings) {
       var btn = $("#js-order-btn", context);
 
-      if (window.location.href.indexOf("created_DESC") > -1) {
+      if (window.location.href.indexOf("_DESC") > -1) {
         btn.addClass("arrow-up");
       }
 
-      $('.bef-checkboxes input:checked').once('viewsExposedForm').each(function() {
+      $('#edit-categorie-collapsible .bef-checkboxes input:checked').once('viewsExposedForm').each(function() {
         var label = $(this)['0'].labels['0'].innerHTML;
+        console.log(label);
         var labelClass = label.toLowerCase().replace(" ", "-");
         var itemId = $(this).attr('id');
         var color = $(this).parent().data('color');
@@ -21,9 +22,13 @@
         $("#js-selected-categories").append(insert);
       });
 
-      btn.once('viewsExposedForm').click(function() {
-        var radioBox = $('input[name="sort_bef_combine"]:not(:checked)', context);
-        radioBox.trigger( "click" );
+      $('body').once('viewsExposedForm').on('click', '#js-order-btn', function() {
+        var fieldSet = $('fieldset[data-drupal-selector="edit-sort-bef-combine"]', context)
+
+        fieldSet.each(function () {
+          let inputUncheck = $(this).find('input:not(:checked)');
+          inputUncheck.trigger( "click" );
+        });
       });
     }
   };
