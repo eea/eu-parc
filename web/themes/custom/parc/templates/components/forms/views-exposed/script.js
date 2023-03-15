6a4df78a71fd728d0a0b11f7109b20ea66d7d3b9
Nick Procopan <nick@optasy.com>
2023-03-15 18:30:31 +0200

    news/events filters

--- web/themes/custom/parc/templates/components/forms/views-exposed/script.js
+++ web/themes/custom/parc/templates/components/forms/views-exposed/script.js
@@ -7,28 +7,33 @@
   Drupal.behaviors.viewsExposedForm = {
     attach: function (context, settings) {
       var btn = $("#js-order-btn", context);
-
       if (window.location.href.indexOf("_DESC") > -1) {
         btn.addClass("arrow-up");
       }
 
-      $('#edit-category-collapsible .bef-checkboxes input:checked').once('viewsExposedForm').each(function() {
+      $(context).find("details[data-drupal-selector='edit-category-collapsible'] .bef-checkboxes input").each(function() {
         var label = $(this)['0'].labels['0'].innerHTML;
         var labelClass = label.toLowerCase().replace(" ", "-");
         var itemId = $(this).attr('id');
         var color = $(this).parent().data('color');
-        var insert = '<label for="' + itemId + '" class="option bg--' + labelClass + '" style="--ci-bg: ' + color + '">' + label + '</label>'
-        $("#js-selected-category").append(insert);
+        if ($(this).is(':checked')) {
+          if ($(context).find("#js-selected-category label[class*='bg--" + labelClass + "']").length === 0) {
+            var insert = '<label for="' + itemId + '" class="option bg--' + labelClass + '" style="--ci-bg: ' + color + '">' + label + '</label>'
+            $("#js-selected-category").append(insert);
+          }
+        }
+        else {
+          $(context).find("#js-selected-category label[class*='bg--" + labelClass + "']").remove();
+        }
+
       });
-
-      $('body').once('viewsExposedForm').on('click', '#js-order-btn', function() {
-        var fieldSet = $('fieldset[data-drupal-selector="edit-sort-bef-combine"]', context)
-
-        fieldSet.each(function () {
-          let inputUncheck = $(this).find('input:not(:checked)');
-          inputUncheck.trigger( "click" );
+      $(context).find("#js-selected-category label").each(function () {
+        $(this).on('click', function() {
+          let idAttr = $(this).attr('for');
+          $(context).find("details[data-drupal-selector='edit-category-collapsible'] .bef-checkboxes input#" + idAttr).prop("checked", false).trigger("change");
+          $(this).remove();
         });
-      });
+      })
     }
   };
 
