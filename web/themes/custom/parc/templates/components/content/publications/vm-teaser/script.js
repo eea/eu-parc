4ed32edcf48d997105e26f62a986a043efb0c40e
Max <max@selk.global>
2023-03-24 15:18:08 +0200

    changed Thematic areas

--- web/themes/custom/parc/templates/components/content/publications/vm-teaser/script.js
+++ web/themes/custom/parc/templates/components/content/publications/vm-teaser/script.js
@@ -6,26 +6,25 @@
 (function ($, Drupal, once) {
   Drupal.behaviors.keyMessagesCarousel = {
     attach: function (context) {
-      $(context).find('.js-slider-prev').once('keyMessagesCarousel').on('click', function () {
-        updateSlidePosition("prev");
+      $(context).find('.js-slider-prev').once('keyMessagesCarousel').on('click', function (e) {
+        updateSlidePosition(e, "prev");
       });
 
-      $(context).find('.js-slider-next').once('keyMessagesCarousel').on('click', function () {
-        updateSlidePosition("next");
+      $(context).find('.js-slider-next').once('keyMessagesCarousel').on('click', function (e) {
+        updateSlidePosition(e, "next");
       });
     }
   };
-})(jQuery, Drupal, once);
 
+  function updateSlidePosition(func, direction) {
+    let cs = func.currentTarget.parentNode.querySelector(".carousel-scroll");
+    const firstSlideWidth = cs.querySelector(".cs--item").offsetWidth;
+    console.log(firstSlideWidth);
 
-
-function updateSlidePosition(direction) {
-  let cs = document.querySelector(".carousel-scroll");
-  const firstSlideWidth = cs.querySelector(".cs--item").offsetWidth;
-
-  if (direction === "prev") {
-    cs.scrollLeft -= firstSlideWidth;
-  } else {
-    cs.scrollLeft += firstSlideWidth;
+    if (direction === "prev") {
+      cs.scrollLeft -= firstSlideWidth;
+    } else {
+      cs.scrollLeft += firstSlideWidth;
+    }
   }
-}
+})(jQuery, Drupal, once);
