790e03c76ce560a7fd60475ac670829bb47a6ff9
nickedwro <121108836+nickedwro@users.noreply.github.com>
2023-03-30 17:44:18 +0300

    Fe (#223)
    * Event info align, Thematic area meniu dropdown, main menu padding remove
    
    * New fixes for SP
    
    ---------
    
    Co-authored-by: Max <max@selk.global>

--- web/themes/custom/parc/templates/components/content/publications/vm-default/script.js
+++ web/themes/custom/parc/templates/components/content/publications/vm-default/script.js
@@ -27,4 +27,29 @@
       cs.scrollLeft += firstSlideWidth;
     }
   }
+
+  Drupal.behaviors.publicationUrl = {
+    attach: function (context,) {
+      hash = window.location.hash;
+      $(context).find(hash).once('publicationUrl').parents('.collapse').addClass('show');
+
+      $('.js-copy-to-clipboard').click(function (e) {
+        var btn = $(this);
+        e.preventDefault();
+        var copyText = $(this).attr('href');
+
+        btn.addClass('added');
+        setTimeout(function () {
+          btn.removeClass('added');
+        }, 1000);
+
+        document.addEventListener('copy', function(e) {
+          copyText = window.location.origin + copyText;
+           e.clipboardData.setData('text/plain', copyText);
+           e.preventDefault();
+        }, true);
+        document.execCommand("copy");
+      });
+    }
+  }
 })(jQuery, Drupal, once);
