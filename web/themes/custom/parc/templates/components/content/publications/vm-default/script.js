2dc3b1b6b9ccf618f6b0dfdb0a16da3925e9f5d9
Max <max@selk.global>
2023-03-31 12:43:19 +0300

    Fixed bug with jquery

--- web/themes/custom/parc/templates/components/content/publications/vm-default/script.js
+++ web/themes/custom/parc/templates/components/content/publications/vm-default/script.js
@@ -48,11 +48,11 @@
     attach: function (context,) {
       hash = window.location.hash;
 
-      if (hash.indexOf('key--messages')) {
+      if (hash.indexOf('key--messages') !== -1) {
         $(context).find(hash).once('publicationUrl').parents('.collapse').addClass('show');
       }
 
-      if (hash.indexOf('scientific-publications')) {
+      if (hash.indexOf('scientific-publications-') !== -1) {
         const node = hash + ' .collapse';
         $(context).find(node).once('publicationUrl').addClass('show');
       }
