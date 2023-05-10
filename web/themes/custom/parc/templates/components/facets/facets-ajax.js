9b203b67d058ed1832a3b7d55e38a9780216136f
Stefan Butura <stefanbutura9@gmail.com>
2023-05-10 13:35:58 +0300

    Work on facets

---
+++ web/themes/custom/parc/templates/components/facets/facets-ajax.js
@@ -0,0 +1,31 @@
+(function ($, Drupal, drupalSettings) {
+  Drupal.behaviors.parcFacetsAjax = {
+    attach: function (context, settings) {
+      const facetsAjaxUrl = '/facets-block-ajax';
+      window.facetsCollapseSettings = window.facetsCollapseSettings || {};
+
+      // Store collapse information in a variable.
+      $(document).ajaxSend(function(e, xhr, settings) {
+        if (settings.url.startsWith(facetsAjaxUrl)) {
+          $('.block-facets .collapse').each(function () {
+            let id = $(this).attr('id');
+            window.facetsCollapseSettings[id] = $(this).hasClass('show');
+          });
+        }
+      });
+
+      // On facets AJAX complete, apply previously stored collapse information.
+      $(document).once('parcFacetsCollapse').ajaxComplete(function(e, xhr, settings) {
+        if (settings.url.startsWith(facetsAjaxUrl)) {
+          $.each(window.facetsCollapseSettings, function (key, value) {
+            let show = 'hide';
+            if (value) {
+              show = 'show';
+            }
+            $('#' + key).collapse(show);
+          });
+        }
+      });
+    },
+  };
+})(jQuery, Drupal, drupalSettings);
