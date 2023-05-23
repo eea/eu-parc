dc66e1f3ac783810ff3186fed819fb05a77d172a
Stefan Butura <stefanbutura9@gmail.com>
2023-05-23 19:30:02 +0300

    Work on the calendar display for the event page

---
+++ web/themes/custom/parc/templates/components/carousel/carousel-scroll.js
@@ -0,0 +1,46 @@
+/**
+ * @file
+ * Description.
+ */
+
+(function ($, Drupal, once) {
+  Drupal.behaviors.carouselScroll = {
+    attach: function (context) {
+      $(context).find('.js-slider-prev').once('carouselScroll').on('click', function (e) {
+        updateCarouselSlidePosition(e, "prev");
+      });
+
+      $(context).find('.js-slider-next').once('carouselScroll').on('click', function (e) {
+        updateCarouselSlidePosition(e, "next");
+      });
+    }
+  };
+
+  function updateCarouselSlidePosition(func, direction) {
+    let cs = func.currentTarget.parentNode.querySelector(".carousel-scroll");
+    const firstSlideWidth = cs.querySelector(".cs--item").offsetWidth;
+    const parentWidth = cs.offsetWidth;
+    const count = cs.childElementCount;
+
+    let btnNext = cs.parentElement.querySelector(".js-slider-next");
+    let btnPrev = cs.parentElement.querySelector(".js-slider-prev");
+
+    if (direction === "prev") {
+      btnNext.classList.remove('d-none');
+      if (cs.scrollLeft - firstSlideWidth <= 21) {
+        btnPrev.classList.add('d-none');
+      } else {
+        btnPrev.classList.remove('d-none');
+      }
+      cs.scrollLeft -= firstSlideWidth;
+    } else {
+      btnPrev.classList.remove('d-none');
+      if (cs.scrollLeft + firstSlideWidth > count * firstSlideWidth - parentWidth) {
+        btnNext.classList.add('d-none');
+      } else {
+        btnNext.classList.remove('d-none');
+      }
+      cs.scrollLeft += firstSlideWidth;
+    }
+  }
+})(jQuery, Drupal, once);
