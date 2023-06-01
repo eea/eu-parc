bcaa362af88a1459ce715154deb1dd4dd1d382d3
Stefan Butura <stefanbutura9@gmail.com>
2023-06-01 11:07:26 +0300

    Update core, libraries

--- web/libraries/ckeditor/plugins/notification/lang/el.js
+++ web/libraries/ckeditor/plugins/notification/lang/el.js
@@ -2,9 +2,6 @@
 Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
 For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 */
-CKEDITOR.plugins.setLang( 'fakeobjects', 'cy', {
-	anchor: 'Angor',
-	hiddenfield: 'Maes Cudd',
-	iframe: 'IFrame',
-	unknown: 'Gwrthrych Anhysbys'
+CKEDITOR.plugins.setLang( 'notification', 'el', {
+	closed: 'Η ειδοποίηση έκλεισε.'
 } );
