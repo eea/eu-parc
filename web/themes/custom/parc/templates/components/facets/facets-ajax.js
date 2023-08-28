3e6b17d47aa5a7ba0556c99826d4ddf1d2b78b41
Stefan Butura <stefanbutura9@gmail.com>
2023-08-28 14:35:08 +0300

    #255701 - updates to maps

--- web/themes/custom/parc/templates/components/facets/facets-ajax.js
+++ web/themes/custom/parc/templates/components/facets/facets-ajax.js
@@ -1,49 +1,17 @@
 (function ($, Drupal, drupalSettings) {
   Drupal.behaviors.parcFacetsAjax = {
     attach: function (context, settings) {
-      const facetsAjaxUrl = '/facets-block-ajax';
-      window.facetsCollapseSettings = window.facetsCollapseSettings || {};
-
-      // Store collapse information in a variable.
-      $(document).ajaxSend(function(e, xhr, settings) {
-        if (settings.url.startsWith(facetsAjaxUrl)) {
-          $('.block-facets .collapse').each(function () {
-            let id = $(this).attr('id');
-            window.facetsCollapseSettings[id] = $(this).hasClass('show');
-          });
-        }
-      });
-
-      // On facets AJAX complete, apply previously stored collapse information.
-      $(document).once('parcFacetsCollapse').ajaxComplete(function(e, xhr, settings) {
-        if (settings.url.startsWith(facetsAjaxUrl)) {
-          $.each(window.facetsCollapseSettings, function (key, value) {
-            let show = 'hide';
-            if (value) {
-              show = 'show';
-            }
-            $('#' + key).collapse(show);
-          });
-        }
-      });
-
-      $(document).ready(function(){
-        var windowWidth = jQuery(window).width();
-        if (windowWidth <= 767) {
-          $('#collapsecountry').collapse('hide');
-          $('#collapsecategory').collapse('hide');
-          $('#collapselab_type').collapse('hide');
-          $('#collapselab_sampling_type').collapse('hide');
-          $('#collapselab_substance_group').collapse('hide');
-        }
-      });
-
-      $('.facets-widget-parc_checkbox li.collapse').once('facetCollapseExpand').on('shown.bs.collapse', function () {
-        $(this).prev().addClass('facet-collapse-expanded');
-      });
-
-      $('.facets-widget-parc_checkbox li.collapse').once('facetCollapseCollapse').on('hidden.bs.collapse', function () {
-        $(this).prev().removeClass('facet-collapse-expanded');
+      $(document).ready(function () {
+        once('collapseFacets', 'body').forEach(() => {
+          var windowWidth = $(window).width();
+          if (windowWidth <= 767) {
+            $('#collapsefacet-blocklab-type').collapse('hide');
+            $('#collapsefacet-blocklab-sampling-type').collapse('hide');
+            $('#collapsefacet-blocklab-substance-group').collapse('hide');
+            $('#collapsefacet-blockcategory').collapse('hide');
+            $('#collapsefacet-blockcountry').collapse('hide');
+          }
+        });
       });
     },
   };
