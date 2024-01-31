f7ef2578a7628c5e6fee04cf37b77ee27cef3d87
Stefan Butura <stefanbutura9@gmail.com>
2024-01-31 19:27:29 +0200

    WIP Projects

--- web/modules/custom/parc_core/js/projects.js
+++ web/modules/custom/parc_core/js/projects.js
@@ -1,34 +1,116 @@
-(function ($, Drupal, once) {
+(function ($, Drupal, once, drupalSettings) {
 
   Drupal.behaviors.parcProjects = {
     attach: function (context, settings) {
-// draw a curvy line between point (startX,startY) and point (endX,endY)
-      function drawCurve(startX, startY, endX, endY, id) {
+      var topic_counts = [];
+      var keyword_counts = [];
+      var project_keywords_counts = [];
+      var project_topics_counts = [];
 
-        // exemple of a path: M318,345 L330,345 C450,345 380,124 504,124 L519,124
+      var keywords = settings.parc_projects.keywords ?? {};
+      var topics = settings.parc_projects.topics ?? {};
+      var projects_list = settings.parc_projects.projects_list ?? {};
 
+      var div_spacing = 10;
+
+      function drawProjectsBlock() {
+        topic_counts = [];
+        keyword_counts = [];
+        project_keywords_counts = [];
+        project_topics_counts = [];
+
+        let relations = settings.parc_projects.relations ?? {};
+
+        $.each(relations, function (key, value) {
+          let project = value.project;
+          let term = value.term;
+          let type = value.type
+          drawCurve(project, term, type);
+        });
+      }
+
+      function drawCurve(project, term, type) {
+        let project_div = $('.projects-chart div[data-project="' + project + '"]');
+        let term_div = $('.projects-chart div[data-' + type + '="' + term  + '"]');
+        let path_element = $('.projects-chart path[data-project="' + project + '"][data-' + type + '="' + term + '"]');
+
+        let startX;
+        let startY;
+        let endX;
+        let endY;
+        let spacing;
+        let total;
+
+        if (type == 'topic') {
+          if (!topic_counts[term]) {
+            topic_counts[term] = 1;
+          }
+          if (!project_topics_counts[project]) {
+            project_topics_counts[project] = 1;
+          }
+          spacing = 30;
+          endX = project_div.position().left - div_spacing;
+          endY = project_div.position().top + project_div.outerHeight() / 2;
+          total = projects_list[project].topics.length;
+          endY -= ((total + 1) / 2 - project_topics_counts[project]) * spacing / 3;
+
+          startX = term_div.position().left + div_spacing;
+          startX += term_div.outerWidth();
+
+          startY = term_div.position().top + term_div.outerHeight() / 2;
+          total = topics[term].count;
+          startY -= ((total + 1) / 2 - topic_counts[term]) * spacing
+
+          topic_counts[term]++;
+          project_topics_counts[project]++;
+        }
+        else {
+          if (!project_keywords_counts[project]) {
+            project_keywords_counts[project] = 1;
+          }
+          if (!keyword_counts[term]) {
+            keyword_counts[term] = 1;
+          }
+
+          spacing = 3;
+          startX = project_div.position().left + div_spacing;
+          startX += project_div.outerWidth();
+
+          startY = project_div.position().top + project_div.outerHeight() / 2;
+          total = projects_list[project].keywords.length;
+          startY -= ((total + 1) / 2 - project_keywords_counts[project]) * spacing;
+
+          endX = term_div.position().left - div_spacing;
+          endY = term_div.position().top + term_div.outerHeight() / 2;
+
+          total = keywords[term].count;
+          endY -= ((total + 1) / 2 - keyword_counts[term]) * spacing * 2;
+
+          project_keywords_counts[project]++;
+          keyword_counts[term]++;
+        }
+
+        // https://stackoverflow.com/questions/45240401/svg-path-create-a-curvy-line-to-link-two-points
         // M
         var AX = startX;
-        console.log(AX);
         var AY = startY;
 
         // L
-        var BX = Math.abs(endX - startX) * 0.05 + startX;
+        var BX = Math.abs(endX - startX) * 0.01 + startX;
         var BY = startY;
 
         // C
-        var CX = startX + Math.abs(endX - startX) * 0.33;
+        var CX = (endX - startX) * 0.75 + startX;
         var CY = startY;
-        var DX = endX - Math.abs(endX - startX) * 0.33;
+        var DX = (endX - startX) * 0.25 + startX;
         var DY = endY;
-        var EX = - Math.abs(endX - startX) * 0.05 + endX;
+        var EX = - Math.abs(endX - startX) * 0.01 + endX;
         var EY = endY;
 
         // L
         var FX = endX;
         var FY = endY;
 
-        // setting up the path string
         var path = 'M' + AX + ',' + AY;
         path += ' L' + BX + ',' + BY;
         path +=  ' ' + 'C' + CX + ',' + CY;
@@ -36,17 +118,66 @@
         path += ' ' + EX + ',' + EY;
         path += ' L' + FX + ',' + FY;
 
-
-        // applying the new path to the element
-        document.getElementById(id).setAttribute("d", path);
-
+        path_element.attr('d', path);
       }
 
-//drawCurve(200,400, 519,124);
-      drawCurve(100,100, 500,500, 'myPath');
-      drawCurve(100,120, 500,480, 'myPath2');
-      drawCurve(100,200, 500,410, 'myPath3');
+      $(document).ready(function () {
+        drawProjectsBlock();
+      });
+
+      $(window).resize(function() {
+        drawProjectsBlock();
+      });
+
+      function focusProject(project_id, topic_id, keyword_id) {
+        $('.projects-chart path[data-project="' + project_id + '"]').css('opacity', '1');
+        $('.projects-chart div[data-project="' + project_id + '"]').css('opacity', '1');
+        $.each(projects_list[project_id].topics, function (key, value) {
+          $('.projects-chart div[data-topic="' + value + '"]').css('opacity', '1');
+        });
+        $.each(projects_list[project_id].keywords, function (key, value) {
+          $('.projects-chart div[data-keyword="' + value + '"]').css('opacity', '1');
+        });
+
+        if (topic_id) {
+          $('.projects-chart path[data-topic]:not([data-topic="' + topic_id +'"])').css('opacity', '.1');
+          $('.projects-chart .project-topic:not([data-topic="' + topic_id + '"])').css('opacity', '.1');
+        }
+        if (keyword_id) {
+          $('.projects-chart path[data-keyword]:not([data-keyword="' + keyword_id +'"])').css('opacity', '.1');
+          $('.projects-chart .project-keyword:not([data-keyword="' + keyword_id + '"])').css('opacity', '.1');
+        }
+      }
+
+      $(once('projectsHoverable', '.projects-chart .projects-hoverable, .projects-chart .projects-hoverable-path', context)).each(function () {
+        $(this).on('mouseenter', function () {
+          $('.projects-chart [data-project],.projects-chart [data-keyword],.projects-chart [data-topic]').css('opacity', '.1');
+          let parent = $(this).hasClass('projects-hoverable-path') ? $(this) : $(this).parent();
+          let project_id = parent.attr('data-project')
+          let keyword_id = parent.attr('data-keyword')
+          let topic_id = parent.attr('data-topic')
+
+          if (project_id) {
+            focusProject(project_id, topic_id, keyword_id);
+          }
+          else if (keyword_id) {
+            $('.projects-chart [data-keyword="' + keyword_id + '"]').css('opacity', '1');
+            $.each(keywords[keyword_id].projects, function(key, value) {
+              focusProject(value, null, keyword_id);
+            });
+          }
+          else if (topic_id) {
+            $('.projects-chart [data-topic="' + topic_id + '"]').css('opacity', '1');
+            $.each(topics[topic_id].projects, function(key, value) {
+              focusProject(value, topic_id, null);
+            });
+          }
+        });
+        $(this).on('mouseleave', function () {
+          $('.projects-chart [data-project],.projects-chart [data-keyword],.projects-chart [data-topic]').css('opacity', '');
+        });
+      });
     }
   }
 
-})(jQuery, Drupal, once);
+})(jQuery, Drupal, once, drupalSettings);
--- web/modules/custom/parc_core/src/Plugin/Block/ParcProjectBlock.php
+++ web/modules/custom/parc_core/src/Plugin/Block/ParcProjectBlock.php
@@ -3,15 +3,19 @@
 namespace Drupal\parc_core\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\Cache;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\geolocation\MapCenterManager;
+use Drupal\geolocation\MapProviderManager;
+use Drupal\node\NodeInterface;
 use Drupal\parc_core\ParcSearchManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
- * Provides a block to display the search results.
+ * Provides a block to display the parc projects.
  *
  * @Block(
  *   id = "parc_projects",
@@ -19,20 +23,195 @@ use Symfony\Component\HttpFoundation\RequestStack;
  *   category = @Translation("PARC"),
  * )
  */
-class ParcProjectBlock extends BlockBase {
+class ParcProjectBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Construct a ParcProjectBlock instance.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param string $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
+    $projects = $this->entityTypeManager->getStorage('node')
+      ->loadByProperties([
+        'type' => 'project',
+        'status' => TRUE,
+      ]);
+
+    usort($projects, function (NodeInterface $a, NodeInterface $b) {
+      $topic_a = $a->get('field_project_topics')->entity;
+      $topic_b = $b->get('field_project_topics')->entity;
+
+      $count_a = $a->get('field_project_topics')->count();
+      $count_b = $b->get('field_project_topics')->count();
+
+      $weight_a = $topic_a->get('weight')->value;
+      $weight_b = $topic_b->get('weight')->value;
+
+      if ($count_a != $count_b) {
+        return $count_a <=> $count_b;
+      }
+
+      if ($weight_a != $weight_b) {
+        return $weight_a <=> $weight_b;
+      }
+
+      return strcasecmp($a->getTitle(), $b->getTitle());
+    });
+
+    $topics = [];
+    $keywords = [];
+    $projects_list = [];
+
+    $relations = [];
+    foreach ($projects as $project) {
+      if ($project->get('field_project_topics')->isEmpty()) {
+        continue;
+      }
+
+      $project_topics = [];
+      foreach ($project->get('field_project_topics') as $item) {
+        /** @var \Drupal\taxonomy\TermInterface $topic */
+        $topic = $item->entity;
+        $project_topics[] = $topic->id();
+        if (empty($topics[$topic->id()])) {
+          $topics[$topic->id()] = [
+            'count' => 0,
+            'name' => $topic->label(),
+            'id' => $topic->id(),
+            'color' => $topic->get('field_color')->color,
+          ];
+        }
+        $topics[$topic->id()]['count']++;
+        $topics[$topic->id()]['projects'][] = $project->id();
+      }
+
+      if ($project->get('field_project_keywords')->isEmpty()) {
+        continue;
+      }
+
+      $project_keywords = [];
+      foreach ($project->get('field_project_keywords') as $item) {
+        /** @var \Drupal\taxonomy\TermInterface $keyword */
+        $keyword = $item->entity;
+        $project_keywords[] = $keyword->id();
+        if (empty($keywords[$keyword->id()])) {
+          $keywords[$keyword->id()] = [
+            'count' => 0,
+            'name' => $keyword->label(),
+            'id' => $keyword->id(),
+            'color' => $keyword->get('field_color')->color,
+          ];
+        }
+        $keywords[$keyword->id()]['count']++;
+        $keywords[$keyword->id()]['projects'][] = $project->id();
+      }
+
+      $projects_list[$project->id()] = [
+        'name' => $project->label(),
+        'keywords' => $project_keywords,
+        'topics' => $project_topics,
+        'percentage' => 100 / count($projects),
+        'id' => $project->id(),
+        'url' => $project->toUrl()->toString(),
+      ];
+
+      foreach ($project_topics as $topic) {
+        $relations[] = [
+          'term' => $topic,
+          'project' => $project->id(),
+          'type' => 'topic',
+          'color' => $topics[$topic]['color'],
+        ];
+      }
+      foreach ($project_keywords as $keyword) {
+        $relations[] = [
+          'term' => $keyword,
+          'project' => $project->id(),
+          'type' => 'keyword',
+          'color' => $keywords[$keyword]['color'],
+        ];
+      }
+    }
+
+    foreach ($topics as &$topic) {
+      $topic['percentage'] = $topic['count'] / count($projects) * 100;
+    }
+    foreach ($keywords as &$keyword) {
+      $keyword['percentage'] = $keyword['count'] / count($projects) * 100;
+    }
+
     return [
-      '#attached' => [
-        'library' => [
-          'parc_core/projects',
+      'desktop' => [
+        '#attached' => [
+          'library' => [
+            'parc_core/projects',
+          ],
+          'drupalSettings' => [
+            'parc_projects' => [
+              'relations' => $relations,
+              'topics' => $topics,
+              'keywords' => $keywords,
+              'projects_list' => $projects_list
+            ],
+          ],
+        ],
+        '#theme' => 'parc_projects',
+        '#topics' => $topics,
+        '#keywords' => $keywords,
+        '#projects' => $projects_list,
+      ],
+      'mobile' => [
+        '#type' => 'view',
+        '#name' => 'projects',
+        '#display_id' => 'block_1',
+        '#attributes' => [
+          'class' => ['mobile-projects'],
         ],
       ],
-      '#theme' => 'parc_projects',
     ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheTags() {
+    return Cache::mergeTags(parent::getCacheTags(), [
+      'node_list:projects',
+    ]);
   }
 
 }
