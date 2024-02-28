686687eb4fdf6abe93149df60d2d23a64d22b241
Stefan Butura <stefanbutura9@gmail.com>
2024-02-29 00:00:36 +0200

    Work on publication scroll

--- web/themes/custom/parc/templates/components/views/publications/script.js
+++ web/themes/custom/parc/templates/components/views/publications/script.js
@@ -25,22 +25,22 @@
         });
       }
 
+      $(document).ready(function () {
+        setTimeout(function () {
+          const id = window.location.hash;
+          if (id) {
+            let item1 = $('.left-col ' + id);
+            let item2 = $('.right-col ' + id);
 
-
-      setTimeout(function () {
-        const id = window.location.hash;
-        if (id) {
-          let item1 = $('.left-col ' + id);
-          let item2 = $('.right-col ' + id);
-
-          if (window.innerWidth >= 1280 && item2.length) {
-            $('html, body').animate({scrollTop: item2.offset().top - 100}, 100);
+            if (window.innerWidth >= 1280 && item2.length) {
+              $('html, body').animate({scrollTop: item2.offset().top - 100}, 100);
+            }
+            else {
+              $('html, body').animate({scrollTop: item1.offset().top - 100}, 100);
+            }
           }
-          else {
-            $('html, body').animate({scrollTop: item1.offset().top - 100}, 100);
-          }
-        }
-      }, 1);
+        }, .3);
+      });
 
       $(once('viewPublications', '.views--2col', context)).each(function() {
         setPublicationsHeight($(this));
