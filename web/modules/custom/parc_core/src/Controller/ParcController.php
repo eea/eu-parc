3fca8e7a5a948329248e6272a4c2a360779fa2bd
Stefan Butura <stefanbutura9@gmail.com>
2024-04-22 17:19:24 +0300

    Increase event og image size

--- web/modules/custom/parc_core/src/Controller/ParcController.php
+++ web/modules/custom/parc_core/src/Controller/ParcController.php
@@ -78,19 +78,19 @@ class ParcController extends ControllerBase implements ContainerInjectionInterfa
    *   The HTML response.
    */
   public function eventOgImage(NodeInterface $node, $type = 'standard') {
-    $width = 800;
-    $height = 420;
+    $width = 1200;
+    $height = 630;
 
-    $margin = 26;
-    $margin_bottom = str_contains($type, 'standard') ? 0 : 66;
+    $margin = 40;
+    $margin_bottom = $type == 'standard' ? 0 : 100;
 
     /** @var \Drupal\taxonomy\TermInterface $category */
     $category = $node->get('field_categories')->entity;
     $category_name = $category->getName();
-    $category_font_size = 40;
+    $category_font_size = 60;
 
     $location = $this->eventsManager->getEventLocation($node);
-    $location_font_size = 26;
+    $location_font_size = 40;
 
     $color = $this->request->query->get('color');
     if (empty($color)) {
@@ -114,7 +114,7 @@ class ParcController extends ControllerBase implements ContainerInjectionInterfa
     imagettftext($image_p, $location_font_size, 0, $margin, $height - $location_font_size - $margin_bottom, $white, $font, $location);
 
     // Write date.
-    @imagettftext($image_p, $font_size, 0, $margin, $font_size + $category_font_size + 53, $white, $font, $date);
+    imagettftext($image_p, $font_size, 0, $margin, $font_size + $category_font_size + 80, $white, $font, $date);
 
     ob_start();
     imagejpeg($image_p, NULL, 100);
