756de67110c3c52872f717dfc47fde1dd39a59c0
Stefan Butura <stefanbutura9@gmail.com>
2025-04-24 14:26:24 +0300

    refs #29977 - small css adjustments

--- web/themes/custom/parc/js/script.js
+++ web/themes/custom/parc/js/script.js
@@ -146,21 +146,4 @@
       $autocomplete.autocomplete( "option", "position", { my : "right top-7", at: "right bottom" } );
     }
   };
-
-  Drupal.behaviors.survey = {
-    attach: function (context, settings) {
-      $('.survey-result-percent').each(function () {
-        var $this = $(this);
-        var parent = $this.parent();
-        var percent = parent.attr('data-percent');
-        var parentHeight = parent.outerHeight();
-        var maxHeight = parentHeight - 10;
-
-        var height = Math.round((percent / 100) * maxHeight);
-
-        $this.attr('style', `max-height: ${height}px`);
-      });
-    }
-  }
 })(jQuery, Drupal, once);
-
