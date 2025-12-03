db872e2d1e9142e9877bc1f73dcfefca8acb7aa5
stefanbutura <stefanbutura9@gmail.com>
2025-12-03 18:29:05 +0200

    Parc media page (#288)
    * Work on press releases
    
    * Work on media assets
    
    * Work on media page
    
    * Create paragraphs block
    
    * [Media] Fix images
    
    * [Media] Small fixes
    
    * [Media] Small fixes
    
    * Work on press clippings
    
    * Work on press clippings
    
    * [Media] Small fixes
    
    * Adjust press clippings
    
    * [Press] Work on press releases
    
    * [Media] Slight adjustments
    
    * Press clippings - enable infinite scroll

---
+++ web/themes/custom/parc/js/expand.js
@@ -0,0 +1,32 @@
+/**
+ * Add expand class to elements
+ */
+(function (Drupal, once) {
+  Drupal.behaviors.parcExpand = {
+    attach: function (context, settings) {
+      once('parc-expand', '.js-expand', context).forEach(function (el) {
+        el.addEventListener('click', function (event) {
+          // Ignore clicks on links inside the element
+          if (event.target.closest('a')) {
+            return;
+          }
+
+          const targetValue = el.getAttribute('data-expand-target');
+          if (!targetValue) return;
+
+          const targets = context.querySelectorAll(`[data-expand="${targetValue}"]`);
+          el.classList.toggle('expand');
+          if (el.classList.contains('expand-toggle-parent')) {
+            el.parentElement.classList.toggle('expand');
+          }
+
+          if (targets.length) {
+            targets.forEach(function (target) {
+              target.classList.toggle('expand');
+            });
+          }
+        });
+      });
+    }
+  };
+})(Drupal, once);
