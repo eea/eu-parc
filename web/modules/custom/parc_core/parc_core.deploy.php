97234ae22a102b1c16c474fb9054642a063384dc
Andrei Avarvarei <andrei.a58@yahoo.com>
2026-01-20 10:37:29 +0200

    refs #295147 - Changed field ECTs from integer to decimal

--- web/modules/custom/parc_core/parc_core.deploy.php
+++ web/modules/custom/parc_core/parc_core.deploy.php
@@ -1677,3 +1677,29 @@ function parc_core_deploy_update_hbm_labs_import_2() {
     $result->save();
   }
 }
+
+/**
+ * Migrate values from field_ects to field_ects_decimal.
+ */
+function parc_core_deploy_100007() {
+  $storage = \Drupal::entityTypeManager()->getStorage('node');
+
+  $nids = \Drupal::entityQuery('node')
+    ->accessCheck(FALSE)
+    ->condition('type', 'events')
+    ->execute();
+
+  foreach ($nids as $nid) {
+    $revisions = $storage->revisionIds($storage->load($nid));
+    foreach ($revisions as $vid) {
+      $revision = $storage->loadRevision($vid);
+
+      if ($revision->hasField('field_ects') && $revision->hasField('field_ects_decimal') && !$revision->get('field_ects')->isEmpty()) {
+        $revision->set('field_ects_decimal', (float) $revision->get('field_ects')->value);
+        $revision->setNewRevision(FALSE);
+        $revision->isDefaultRevision(FALSE);
+        $revision->save();
+      }
+    }
+  }
+}
