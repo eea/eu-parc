190f2a49fbe63486ba74dbdf29e947a1c4c9f21b
Stefan Butura <stefanbutura9@gmail.com>
2026-02-03 20:29:18 +0200

    Work on chemicals

--- web/themes/custom/parc/components/chemical-accordion/chemical-accordion.js
+++ web/themes/custom/parc/components/chemical-accordion/chemical-accordion.js
@@ -4,6 +4,32 @@
  */
 
 ((Drupal, once) => {
+  Drupal.behaviors.chemicalAccordionOpen = {
+    attach(context) {
+      const elements = once('chemicals-links', '[data-chemical-target]', context);
+
+      elements.forEach((el) => {
+        const chemicalTarget = el.getAttribute('data-chemical-target');
+        const targetElement = document.querySelector(`[data-chemical="${chemicalTarget}"]`);
+
+        if (!targetElement) {
+          el.classList.add('disabled');
+        }
+
+        el.addEventListener('click', (e) => {
+          e.preventDefault();
+          if (targetElement) {
+            const toggle = targetElement.querySelector('.js-chemical-accordion-toggle');
+            if (toggle && toggle.getAttribute('aria-expanded') !== 'true') {
+              toggle.click();
+            }
+            targetElement.scrollIntoView({behavior: 'smooth'});
+          }
+        });
+      });
+    },
+  };
+
   Drupal.behaviors.chemicalAccordion = {
     attach(context) {
       const accordions = Array.from(context.querySelectorAll('.chemical-accordion'));
